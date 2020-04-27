3deba6196f8a5689cd8b4aba45f9550bb27ff95a
Josh Waller <mdx.programs@gmail.com>
2020-04-26 21:06:18 -0400

    Simplifying boilerplate

--- src/Main.elm
+++ src/Main.elm
@@ -1,161 +1,49 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, h1, h2, input, label, p, text)
-import Html.Attributes exposing (class, placeholder, type_)
-import Html.Events exposing (onClick, onInput)
-import Random
+import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+
+
+
+-- MAIN
 
 
 main =
-    Browser.document
-        { init = init
-        , update = update
-        , view = view
-        , subscriptions = \_ -> Sub.none
-        }
+  Browser.sandbox { init = init, update = update, view = view }
 
 
 
 -- MODEL
 
+type alias Model = Int
 
-type GameState
-    = Initializing
-    | Playing Int Guess
-
-
-type Guess
-    = NotGuessed
-    | Guessed Order
-    | InvalidGuess
-
-
-type alias Model =
-    { guessInput : String, state : GameState }
-
-
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( { guessInput = "", state = Initializing }, generateSecretNumber )
-
+init : Model
+init =
+  0
 
 
 -- UPDATE
 
+type Msg = Increment | Decrement
 
-type Msg
-    = GeneratedNumber Int
-    | GuessChange String
-    | GuessSubmitted
-    | Restart
-
-
-generateSecretNumber : Cmd Msg
-generateSecretNumber =
-    Random.generate GeneratedNumber (Random.int 0 100)
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> Model
 update msg model =
-    case msg of
-        GeneratedNumber num ->
-            ( { model | state = Playing num NotGuessed }, Cmd.none )
+  case msg of
+    Increment ->
+      model + 1
 
-        GuessChange str ->
-            ( { model | guessInput = str }, Cmd.none )
-
-        GuessSubmitted ->
-            case model.state of
-                Playing secretNumber guess ->
-                    let
-                        newGuess =
-                            case String.toInt model.guessInput of
-                                Just num ->
-                                    Guessed (compare num secretNumber)
-
-                                Nothing ->
-                                    InvalidGuess
-                    in
-                    ( { model | state = Playing secretNumber newGuess }, Cmd.none )
-
-                _ ->
-                    ( model, Cmd.none )
-
-        Restart ->
-            ( { model | state = Initializing }, generateSecretNumber )
-
+    Decrement ->
+      model - 1
 
 
 -- VIEW
 
-
-viewGuessInput : Html Msg
-viewGuessInput =
-    div []
-        [ p [] [ text "What's your guess?" ]
-        , input [ type_ "text", onInput GuessChange
-        , class "w-full p-2 border border-green-400 hover:border-green-600 focus:border-green-600 md:flex-1"
-        , class "text-center rounded shadow focus:outline-none focus:shadow-outline"
-        ]
-        []
-        , button [ type_ "button", onClick GuessSubmitted ]
-            [ text "Submit"
-            ]
-        ]
-
-
-viewResult : Order -> Html Msg
-viewResult order =
-    case order of
-        LT ->
-            div []
-                [ p [] [ text "Too Small" ]
-                , viewGuessInput
-                ]
-
-        GT ->
-            div []
-                [ p [] [ text "Too Big" ]
-                , viewGuessInput
-                ]
-
-        EQ ->
-            div []
-                [ p [] [ text "Correct" ]
-                , button
-                    [ type_ "button"
-                    , onClick Restart
-                    ]
-                    [ text "start over" ]
-                ]
-
-
-view : Model -> Browser.Document Msg
+view : Model -> Html Msg
 view model =
-    { title = "PET Stack Guessing Game"
-    , body =
-        [ div []
-            [ h1 [] [ text "PET Stack Guessing Game" ]
-            , div []
-                [ case model.state of
-                    Initializing ->
-                        text "Generating Number"
-
-                    Playing num guess ->
-                        case guess of
-                            NotGuessed ->
-                                viewGuessInput
-
-                            Guessed difference ->
-                                viewResult difference
-
-                            InvalidGuess ->
-                                div []
-                                    [ p [] [ text "You didn't enter a valid number" ]
-                                    , viewGuessInput
-                                    ]
-                ]
-            ]
-        ]
-    }
+  div []
+    [ button [ onClick Decrement, class "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment, class "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" ] [ text "+" ]
+    ]
